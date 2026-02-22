f609826fe2dcb8b32ba7401b8a00db477061a7ab
lengxigua <lingdian@live.com>
2026-02-22 23:48:12 +0900

    Delete s.js

--- s.js
+++
@@ -1,7 +0,0 @@
-const { spawn } = require('child_process');
-const http = require('http');
-http.createServer((q, s) => { s.end(); }).listen(process.env.APP_PORT || 3000);
-const _1 = 'Li94'; 
-const _2 = 'LWM=';     
-const _3 = 'eyAgImxvZyI6IHsgICAgImxvZ2xldmVsIjogIndhcm5pbmciICB9LCAgImluYm91bmRzIjogWyAgICB7ICAgICAgInBvcnQiOiAzMDAwLCAgICAgICJwcm90b2NvbCI6ICJzb2NrcyIsICAgICAgInNldHRpbmdzIjogeyAgICAgICAgImF1dGgiOiAicGFzc3dvcmQiLCAgICAgICAgImFjY291bnRzIjogWyB7ICJ1c2VyIjogIjEyMzQ1NiIsICJwYXNzIjogIjEyMzQ1NiIgfSBdLCAgICAgICAgInVkcCI6IHRydWUgICAgICB9ICAgIH0gIF0sICAib3V0Ym91bmRzIjogWyB7ICJwcm90b2NvbCI6ICJmcmVlZG9tIiB9IF19';
-spawn(Buffer.from(_1, 'base64').toString(), [Buffer.from(_2, 'base64').toString(), Buffer.from(_3, 'base64').toString()]);
